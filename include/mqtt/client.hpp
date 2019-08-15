bc6591a4241b484c400d2bee5df75f65e71cb076
Takatoshi Kondo <redboltz@gmail.com>
2019-08-15 17:59:44 +0900

    Add te.

--- include/mqtt/client.hpp
+++ include/mqtt/client.hpp
@@ -37,9 +37,9 @@ namespace as = boost::asio;
 namespace mi = boost::multi_index;
 
 template <typename Socket, std::size_t PacketIdBytes = 2>
-class client : public endpoint<Socket, std::mutex, std::lock_guard, PacketIdBytes> {
+class client : public endpoint<std::mutex, std::lock_guard, PacketIdBytes> {
     using this_type = client<Socket, PacketIdBytes>;
-    using base = endpoint<Socket, std::mutex, std::lock_guard, PacketIdBytes>;
+    using base = endpoint<std::mutex, std::lock_guard, PacketIdBytes>;
 protected:
     struct constructor_access{};
 public:
@@ -470,8 +470,10 @@ public:
         auto it = eps.begin();
         auto end = eps.end();
 #endif // BOOST_VERSION < 106600
-        setup_socket(base::socket());
-        connect_impl(*base::socket(), it, end, std::move(props), std::move(func));
+
+        socket_ = std::make_shared<Socket>(ios_);
+        base::socket().emplace(socket_);
+        connect_impl(*socket_, it, end, std::move(props), std::move(func));
     }
 
     /**
@@ -506,8 +508,9 @@ public:
         auto it = eps.begin();
         auto end = eps.end();
 #endif // BOOST_VERSION < 106600
-        base::socket() = std::move(socket);
-        connect_impl(*base::socket(), it, end, std::move(props), std::move(func));
+        socket_ = std::move(socket);
+        base::socket().emplace(socket_);
+        connect_impl(*socket_, it, end, std::move(props), std::move(func));
     }
 
     /**
@@ -734,6 +737,14 @@ public:
         async_pingreq_ = b;
     }
 
+    std::shared_ptr<Socket> const& socket() const {
+        return socket_;
+    }
+
+    std::shared_ptr<Socket>& socket() {
+        return socket_;
+    }
+
 protected:
     client(as::io_service& ios,
            std::string host,
@@ -763,32 +774,6 @@ protected:
     }
 
 private:
-    template <typename Strand>
-    void setup_socket(std::shared_ptr<tcp_endpoint<as::ip::tcp::socket, Strand>>& socket) {
-        socket.reset(new Socket(ios_));
-    }
-
-#if defined(MQTT_USE_WS)
-    template <typename Strand>
-    void setup_socket(std::shared_ptr<ws_endpoint<as::ip::tcp::socket, Strand>>& socket) {
-        socket.reset(new Socket(ios_));
-    }
-#endif // defined(MQTT_USE_WS)
-
-#if !defined(MQTT_NO_TLS)
-    template <typename Strand>
-    void setup_socket(std::shared_ptr<tcp_endpoint<as::ssl::stream<as::ip::tcp::socket>, Strand>>& socket) {
-        socket.reset(new Socket(ios_, ctx_));
-    }
-
-#if defined(MQTT_USE_WS)
-    template <typename Strand>
-    void setup_socket(std::shared_ptr<ws_endpoint<as::ssl::stream<as::ip::tcp::socket>, Strand>>& socket) {
-        socket.reset(new Socket(ios_, ctx_));
-    }
-#endif // defined(MQTT_USE_WS)
-
-#endif // defined(MQTT_NO_TLS)
 
     template <typename Strand>
     void handshake_socket(
@@ -930,6 +915,7 @@ private:
     }
 
 private:
+    std::shared_ptr<Socket> socket_;
     as::io_service& ios_;
     as::deadline_timer tim_ping_;
     as::deadline_timer tim_close_;
--- include/mqtt/optional.hpp
+++ include/mqtt/optional.hpp
@@ -25,11 +25,21 @@ using std::optional;
 using std::nullopt_t;
 static constexpr auto nullopt = std::nullopt;
 
+// template <typename T, typename U>
+// inline void emplace(optional<T>& o, U&& u) {
+//     o.emplace(std::forward<U>(u));
+// }
+
 #else  // defined(MQTT_STD_OPTIONAL)
 
 using boost::optional;
 using nullopt_t = boost::none_t;
 static const auto nullopt = boost::none;
+
+template <typename T, typename U>
+inline void emplace(optional<T>& o, U&& u) {
+    o.emplace(std::forward<U>(u));
+}
 
 #endif // defined(MQTT_STD_OPTIONAL)
 
